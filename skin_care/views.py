5d2ae5f40cc0954640ecaebb6597c98f130e05c2
parth <patilparthashok@gmail.com>
2023-02-02 10:55:54 +0530

    added more functionality

--- skin_care/views.py
+++ skin_care/views.py
@@ -11,25 +11,31 @@ def home(request):
 
 def register_page(request):
 	if request.method=='POST':
-	
+		# messages.success(request,"User created !!")
 		username=request.POST['username']
 		email=request.POST['email']
 		password=request.POST['password']
 		confirm_password=request.POST['password1']
 		if password==confirm_password:
 			if User.objects.filter(username=username).exists():
+				messages.error(request,'Username already exists!')
 				return redirect('regi')
 			else:
 				if User.objects.filter(email=email).exists():
+					messages.error(request,'Email already exists!')
 					return redirect('regi')
 				else:
 					user=User.objects.create_user(username=username,email=email,password=password)
 					auth.login(request,user)
 					user.save()
-					return redirect('logi')
+					messages.success(request,"You are registered successfully.")
+					return redirect('regi')
 		
-	else:
+		else:
+			messages.error(request,'Password do not match')
+			return redirect('regi')
 		# return render(request,'skin_care/register.html')
+	else:
 		return render(request,'skin_care/register.html')
 
 # else:
@@ -47,12 +53,17 @@ def login_page(request):
 			auth.login(request,user)
 			return redirect('dash')
 		else:
+			messages.error(request,'Invalid login credentails')
 			return redirect('logi')
 	return render(request,'skin_care/login.html')
 
 def dash_board(request):
-	
-	return render(request,'skin_care/dashboard.html')
+	user_id=request.user.id
+	print(user_id)
+	# pat=User.objects.get(pk=3)
+	pat=User.objects.filter(id=user_id)
+	print(pat)
+	return render(request,'skin_care/dashboard.html',{'pat1':pat})
 
 def log_out(request):
 	if request.method=='POST':
